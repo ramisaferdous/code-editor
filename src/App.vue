9b12efe9c5b2a7a3d378cbaa775ca47adb872bce
ramisaferdous <rifahramisa@iut-dhaka.edu>
2025-01-23 19:34:32 +0600

    code editor using vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>AI-Powered Code Editor</h1>
+    <CodeEditor />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CodeEditor from './components/CodeEditor.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    CodeEditor,
+  },
+};
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
 }
 </style>
